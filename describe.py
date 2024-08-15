0a84ee8da4292be55f0f0bc476bc863376eae644
mhogg <mhogg@student.42.fr>
2024-08-15 13:03:14 +0200

    fix percentile and score, describe is finished

--- describe.py
+++ describe.py
@@ -2,10 +2,7 @@ import pandas as pd
 
 # Remove NaN and not numeric values
 def clean_nan(lst):
-    for i in lst:
-        if i != i or not isinstance(i, (float, int)): 
-            lst.remove(i)
-    return lst
+    return [x for x in lst if isinstance(x, (float, int)) and x == x]
 
 # Count number of non-NA/null observations
 def ft_count(lst):
@@ -33,32 +30,34 @@ def ft_mean(lst):
     sum_lst = 0
     for i in lst:
         sum_lst += i
-    # print(sum_lst)
-    # print(len(lst))
     return sum_lst / len(lst)
 
-# Standard deviation of the observations
+# Standard deviation of the list
 def ft_std(lst):
     return (sum([(x - ft_mean(lst)) ** 2 for x in lst]) / len(lst)) ** 0.5
 
-# 25-percentile score
-# Meaning 25% of the observations are below this value
+# Percentile score
+# Meaning p% of the observations are below this value
+def ft_percentile(lst, p):
+    lst.sort()
+    index = (len(lst) - 1) * p
+    lower = int(index)
+    upper = lower + 1 if lower + 1 < len(lst) else lower
+    
+    if upper == lower:
+        return lst[lower]
+    else:
+        return lst[lower] + (index - lower) * (lst[upper] - lst[lower])
+
 def ft_25(lst):
-    lst.sort()
-    return lst[int(len(lst) * 0.25)]
+    return ft_percentile(lst, 0.25)
 
-# 50-percentile score
-# Meaning 50% of the observations are below this value
 def ft_50(lst):
-    #clean_nan(lst)
-    lst.sort()
-    return lst[int(len(lst) * 0.5)]
+    return ft_percentile(lst, 0.50)
 
-# 75-percentile score
-# Meaning 75% of the observations are below this value
 def ft_75(lst):
-    lst.sort()
-    return lst[int(len(lst) * 0.75)]
+    return ft_percentile(lst, 0.75)
+
 
 # Create dataframe with describe values
 def describe(features):
@@ -66,7 +65,7 @@ def describe(features):
              {"25%": ft_25}, {"50%": ft_50}, {"75%": ft_75}, {"max": ft_max}]
     describe = pd.DataFrame()
     for i in range(len(features.columns)):
-        col = features.iloc[:,i].tolist()
+        col = clean_nan(features.iloc[:,i].tolist())
         for func in funcs:
             for key, value in func.items():
                 describe.loc[key, features.columns[i]] = value(col)
@@ -79,5 +78,9 @@ if __name__ == '__main__':
                    "Transfiguration", "Potions", "Care of Magical Creatures", "Charms", "Flying"]]
     labels = ["Feature " + str(i+1) for i in range(len(features.columns))] 
     features.columns = labels
+    
     print(describe(features))
-    # print(features.describe()) to check with pandas describe function
+    
+    # to check with pandas describe function
+    # print(features.describe()) 
+
