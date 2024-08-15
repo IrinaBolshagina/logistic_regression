90d3613166e77ceaf4d2abddea0cc24787103109
daria-dasha <75936315+daria-dasha@users.noreply.github.com>
2024-08-15 17:32:49 +0200

    Update 1 pair_plot.py

--- pair_plot.py
+++ pair_plot.py
@@ -11,12 +11,15 @@ if __name__ == '__main__':
     except OSError as err:
         print("OS error: {0}".format(err))
     else:
-        features1 = ['Index', 'Hogwarts House', 'First Name', 'Last Name',
+        features = ['Index', 'Hogwarts House', 'First Name', 'Last Name',
                         'Birthday', 'Best Hand', 'Arithmancy', 'Astronomy', 'Herbology',
                         'Defense Against the Dark Arts', 'Divination', 'Muggle Studies',
                         'Ancient Runes', 'History of Magic', 'Transfiguration', 'Potions',
                         'Care of Magical Creatures', 'Charms', 'Flying']
 
         sns.set_theme(style="ticks")
-        sns.pairplot(df[['Hogwarts House', 'History of Magic','Charms', 'Flying']], hue='Hogwarts House')
+        sns.pairplot(df[['Hogwarts House', 'Birthday', 'Best Hand', 'Arithmancy', 'Astronomy', 'Herbology',
+                        'Defense Against the Dark Arts', 'Divination', 'Muggle Studies',
+                        'Ancient Runes', 'History of Magic', 'Transfiguration', 'Potions',
+                        'Care of Magical Creatures', 'Charms', 'Flying']], hue='Hogwarts House')
         plt.show()
